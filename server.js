21c0d6b37ecf88f8ab88dccd5756aabf4e89daf4
karissa <92001465+klkeehan@users.noreply.github.com>
2024-02-28 21:26:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const tmi = require('tmi.js');
 
 const client = new tmi.Client({
     options: { debug: true },
-	channels: [ 'eyedazzler', 'suzyqpid' ],
+	channels: [ 'eyedazzler', 'wrnbt', 'suzyqpid' ],
     identity: {
         username: process.env.TWITCH_BOT_USERNAME,
         password: process.env.TWITCH_OAUTH_TOKEN
@@ -16,18 +16,17 @@ client.connect();
 client.on('message', (channel, tags, message, self) => {
 
     if (!self) {
-        // client.say(channel, `Message "${message}" was sent by ${tags.username}`);
-
         var msg = message.split(' ');
 
         var command = msg[0];
         var argument = msg[1];
+        var argC = msg[2]; // not in use rn
 
         if (command === '=commands') {
             client.say(channel, 'nothing here yet FeelsDankMan');
         }
 
-        if (command === '=echo') {
+        if ((command === '=echo') || (command === '=e')) {
             client.say(channel, argument);
         }
 
@@ -43,36 +42,36 @@ client.on('message', (channel, tags, message, self) => {
             client.say(channel, `https://tools.2807.eu/follows?user=${argument}`);
         }
 
-        if (command === 'c') {
+        if ((command === 'c') || (command === '!c')) {
             client.say(channel, '!continue');
         }
 
-        if (command === 'r') {
+        if ((command === 'r') || (command === '!r')) {
             client.say(channel, '!restart');
         }
 
         /*
-        // mirror update
-        if (command === '=mirror') {
-
-            var link;
-
-            if (argument!=null) {
-                link = 'is this even working';
-                client.say(channel, 'updated mirror ApuApproved')
-            } 
-            
-            else { 
-                client.say(channel, link); 
-            }
+        if ((command === 'wrens') && (tags.username !== 'wrrens')) {
+            client.say(channel, '@wrrens');
         }
         */
 
-        // bw stuff
-        function bwRep(word0) {
-            word = "" + word0;
-            client.say(channel, word);
+        /*
+        // mirror stuff WIP
+        var link;
+
+        const regex = /^https:\/\/wos.gg\/r\/([a-zA-Z0-9-]+)/;
+
+        if (command.match(regex)) {
+            link = command;
+            client.say(channel, 'updated mirror ApuApproved');
         }
+
+        // mirror update
+        if ((command === '=mirror') && (link != null)) {
+            client.say(channel, link);
+        }
+        */
 
         // bw detection
         function bwDetect() {
@@ -90,15 +89,47 @@ client.on('message', (channel, tags, message, self) => {
             var word = "" + bwDetect();
             client.say(channel, word);
         }
+       
+        // bw intervals
+        var id;
+
+        function runBw() {
+            bwRep();
+            id = setInterval(bwRep, 20000);
+            setTimeout(() => { clearInterval(id); }, 60000)
+        }
+
+        function stopBw() {
+            clearInterval(id);
+        }
 
         if ((command === '=bw') || (command === '!bw')) {
-            bwRep();
+            if (id) {
+                clearInterval(id);
+            }
+            runBw();
+        }
 
-            var bwTimer = setInterval(bwRep, 20000);
-            setTimeout(() => { clearInterval(bwTimer); }, 60000 )
+        if (command === '=stop') {
+            stopBw();
+        }
+
+        // define command
+        if ((command === '=define') || (command === '=def') || (command === '!define')) {
+            var word = argument.toLowerCase();
+            var url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+            fetch(url)
+            .then(response => {
+                return response.json();
+            })
+            .then(word => {
+                var def = word[0].meanings[0].definitions[0].definition;
+                client.say(channel, `${argument}: ${def}`);    
+            })
+            .catch(_ => {
+                client.say(channel, `couldn't find a definition for ${argument} FeelsDankMan`);
+            })
         }
     }
-
-	// "Alca: Hello, World!"
-	// console.log(`${tags['display-name']}: ${message}`);
 });
